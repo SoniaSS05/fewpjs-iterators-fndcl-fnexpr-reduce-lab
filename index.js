6d3aa6e128956677cefd76918511b67534031b6b
Sonia <serrano_sonia@hotmail.com>
2021-04-06 19:39:12 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const batteries = (number, currentValue) =>{
+    console.log(number);
+    console.log(currentValue);
+     let totalBatteries =  number + currentValue;
+     return totalBatteries;
+}
+const totalBatteries = batteryBatches.reduce(batteries);
+
+
+
+
+/*
+function batteries(batt){
+    let totalBatteries = 0;
+    batt.forEach(function(batt) {
+        totalBatteries += batt;
+    });
+    console.log(typeof(totalBatteries));
+    return totalBatteries; 
+};
+
+ //let prueba = Batteries(batteryBatches);
+ //console.log(prueba);*/
